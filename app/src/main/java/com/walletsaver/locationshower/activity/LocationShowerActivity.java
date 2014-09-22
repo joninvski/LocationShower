a6aa06d9b4a94975b5fb9ef995e59b3e1376bcad
Joao Trindade <trindade.joao@gmail.com>
2014-09-22 18:45:16 +0100

    Refresh button callback and butterknife injection

--- app/src/main/java/com/walletsaver/locationshower/activity/LocationShowerActivity.java
+++ app/src/main/java/com/walletsaver/locationshower/activity/LocationShowerActivity.java
@@ -7,10 +7,26 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+import com.walletsaver.locationshower.R;
 
 public class LocationShowerActivity extends FullscreenActivity {
 
+    @InjectView(R.id.dummy_button) Button refreshButton;
 
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.dummy_button)
+    public void refreshPosition(Button button) {
+        // TODO - Refresh the user position
+    }
 }
-
