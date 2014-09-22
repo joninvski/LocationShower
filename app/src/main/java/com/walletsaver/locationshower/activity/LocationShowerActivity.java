6157ac049926bee9c0ecb4f2b95ef64d0f7117b6
Joao Trindade <trindade.joao@gmail.com>
2014-09-23 00:34:17 +0100

    Now requesting gps updates

--- app/src/main/java/com/walletsaver/locationshower/activity/LocationShowerActivity.java
+++ app/src/main/java/com/walletsaver/locationshower/activity/LocationShowerActivity.java
@@ -3,6 +3,7 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 
+import com.walletsaver.locationshower.exception.NoProviderException;
 import com.walletsaver.locationshower.R;
 
 import de.keyboardsurfer.android.widget.crouton.Crouton;
@@ -25,12 +27,13 @@ import de.keyboardsurfer.android.widget.crouton.Style;
 import java.util.List;
 
 import timber.log.Timber;
-import com.walletsaver.locationshower.exception.NoProviderException;
 
-public class LocationShowerActivity extends FullscreenActivity {
+public class LocationShowerActivity extends FullscreenActivity implements LocationListener {
 
-    private static final long FIVE_MINS = 5 * 60 * 1000; // Five minutes in milliseconds
-    private static final String TAG = "LocationShowerActivity";
+    private static final long FIVE_MINS = 5 * 60 * 1000;        // Five minutes in milliseconds
+
+    private long mMinTime = 5000;                               // default minimum time between new readings
+    private float mMinDistance = 1000.0f;                       // default minimum distance between old and new readings.
 
     private LocationManager mLocationManager;
     private Location mLastLocationReading;
@@ -46,20 +49,69 @@ public class LocationShowerActivity extends FullscreenActivity {
         ButterKnife.inject(this);
 
         mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (mLocationManager == null)
+        if (mLocationManager == null) /* TODO Handle this error */
             finish();
     }
+
+
+    @Override 
+    public void onLocationChanged(Location currentLocation) {
+ 
+        Timber.d("MINE", "Location is now %s", currentLocation.toString());
+
+        // 1) If there is no last location, keep the current location. 
+        if (mLastLocationReading == null) {
+            mLastLocationReading = currentLocation;
+        } 
+ 
+ 
+        // 2) If the current location is older than the last location, ignore the current location 
+        else if (currentLocation.getTime() < mLastLocationReading.getTime())
+            return; 
+ 
+ 
+        // 3) If the current location is newer than the last locations, keep the current location. 
+        else { 
+            mLastLocationReading = currentLocation;
+        } 
+    } 
+ 
+ 
+    @Override 
+    public void onProviderDisabled(String provider) {
+        // not implemented 
+    } 
+ 
+ 
+    @Override 
+    public void onProviderEnabled(String provider) {
+        // not implemented 
+    } 
+ 
+ 
+    @Override 
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // not implemented 
+    } 
 
     @Override
     protected void onResume() {
         super.onResume();
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, mMinTime, mMinDistance, this);
         try {
             mLastLocationReading = getLastKnownLocation();
+
             showPosition(mLastLocationReading);
         } catch (NoProviderException e) {
             alertNoProvider();
         }
     }
+
+    @Override 
+    protected void onPause() { 
+        mLocationManager.removeUpdates(this);
+        super.onPause(); 
+    } 
 
 
     @OnClick(R.id.dummy_button)
